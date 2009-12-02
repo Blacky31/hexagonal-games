4191d8a42bbbcd01799c98453924fb7277d9ca88
viktorzhuravel <viktorzhuravel@358969aa-d8ce-11de-971a-1339e0424b6a>
2009-12-02 09:33:02 +0000

    Mac client project

---
+++ trunk/MacClient/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Game
+//
+//  Created by Viktor Zhuravel on 12/2/09.
+//  Copyright 2009 Kring. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
